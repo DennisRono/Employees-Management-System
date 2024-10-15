d6d9de9bde0d7cded7303ecd9870a9364ccaec70
Dennis Kibet <dennisrkibet@gmail.com>
2024-10-15 11:06:43 +0300

    implemented employees list

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -8,11 +8,17 @@ import {
 import Home from './routes/Home'
 import ErrorBoundary from './routes/ErrorBoundary'
 import NotFound from './routes/NotFound'
+import AllEmployees from './routes/AllEmployees'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} errorElement={<ErrorBoundary />} />
+      <Route
+        path="/employees"
+        element={<AllEmployees />}
+        errorElement={<ErrorBoundary />}
+      />
       <Route path="*" element={<NotFound />} errorElement={<ErrorBoundary />} />
     </>
   )
