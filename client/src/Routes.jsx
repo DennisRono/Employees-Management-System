c3d8f70ae684527ddedc831b1e1a180d9c215a31
Dennis Kibet <dennisrkibet@gmail.com>
2024-10-16 21:08:33 +0300

    created a customers endpoint to consume the /customers endpoint

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -9,14 +9,26 @@ import Home from './routes/Home'
 import ErrorBoundary from './routes/ErrorBoundary'
 import NotFound from './routes/NotFound'
 import AllEmployees from './routes/AllEmployees'
+import AllCustomers from './routes/AllCustomers'
+import Dashboard from './routes/Dashboard'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} errorElement={<ErrorBoundary />} />
       <Route
+        path="/dash"
+        element={<Dashboard />}
+        errorElement={<ErrorBoundary />}
+      />
+      <Route
         path="/employees"
         element={<AllEmployees />}
+        errorElement={<ErrorBoundary />}
+      />
+      <Route
+        path="/customers"
+        element={<AllCustomers />}
         errorElement={<ErrorBoundary />}
       />
       <Route path="*" element={<NotFound />} errorElement={<ErrorBoundary />} />
---
+++ client/src/routes/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <div className="py-4"></div>
+      <Footer />
+    </>
+  )
+}
+
+export default Dashboard
