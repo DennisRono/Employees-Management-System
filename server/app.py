23683577e62235ab0967b77aff0c01168cacc1e7
Your Name <princesslorna2000@gmail.com>
2024-10-16 09:13:46 +0300

    admin model

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify,make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from models import db, Employee, Department, Role,Customer
+from models import db, Employee, Department, Role,Customer,Administrator
 from config import Config
 
 app = Flask(__name__)
@@ -162,6 +162,47 @@ class CustomerResource(Resource):
         db.session.delete(customer)
         db.session.commit()
         return make_response(jsonify({"message": f"Customer {customer.id} deleted"}), 204)
+    
+
+class AdministratorResource(Resource):
+    def get(self, admin_id=None):
+     
+ 
+     if admin_id:
+         administrator= Administrator.query.filter(Administrator.id==admin_id).first()
+         if administrator:
+             return make_response(jsonify(administrator.to_dict()), 200)
+         else:
+             return make_response(jsonify({"error":f"Admistrator {administrator.id} not found"}), 404)
+     else:
+         administrators=Administrator.query.all()
+         return make_response(jsonify([admin.to_dict() for admin in administrators]), 200)
+     
+    def post(self):        
+        data = request.get_json()
+        new_admin = Administrator(admin_name=data["customer_name"], email=data["email"], phone_number=data["phone_number"])
+        db.session.add(new_admin)
+        db.session.commit()
+        return make_response(jsonify(new_admin.to_dict()), 201)
+    
+    def patch(self, admin_id):
+        admin = Administrator.query.filter(Administrator.id==admin_id).first()
+        if not admin:
+            return make_response(jsonify({"error": "Admin not found"}), 404)
+        data = request.get_json()
+        admin.name = data.get("first_name", admin.first_name)
+        admin.last_name = data.get("last_name", admin.last_name)
+        admin.email = data.get("email", admin.email)
+        admin.phone_number = data.get("phone_number", admin.phone_number)
+        db.session.commit()
+        return make_response(jsonify(admin.to_dict()), 200)
+
+    
+    def delete(self, admin_id):        
+        admin = Administrator.query.filter(Administrator.id==admin_id).first()
+        db.session.delete(admin)
+        db.session.commit()
+        return make_response(jsonify({"message": f"Administrator {admin.id} deleted"}), 204)
      
     
 
@@ -171,6 +212,8 @@ api.add_resource(EmployeeResource, "/employees", "/employees/<int:employee_id>")
 api.add_resource(DepartmentResource, "/departments", "/departments/<int:department_id>")
 api.add_resource(RoleResource, "/roles", "/roles/<int:role_id>")
 api.add_resource(CustomerResource, "/customers", "/customers/<int:id>")
+api.add_resource(AdministratorResource, "/administrators", "/administrators/<int:id>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
