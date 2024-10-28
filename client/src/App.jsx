bfc0d4ea8650037a1daa36a349a8baf8494c07ee
Dennis Kibet <dennisrkibet@gmail.com>
2024-10-28 19:46:13 +0300

    adding env var

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
+  console.log(process.env.REACT_APP_BACKEND_URL)
   return (
     <>
       <ToastContainer />
